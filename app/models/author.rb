e0c24267853bbaf58d9ac320a3b1221da30a2f7c
Daniel Santander <danyvo1d@gmail.com>
2020-05-06 11:30:03 -0500

    Add authentication features to blog app

---
+++ app/models/author.rb
@@ -0,0 +1,9 @@
+class Author < ApplicationRecord
+  authenticates_with_sorcery!
+  validates_confirmation_of :password, message: "should match confirmation", if: :password
+end
+
+#class Author < ActiveRecord::Base
+ # authenticates_with_sorcery!
+  #validates_confirmation_of :password, message: "should match confirmation", if: :password
+#end
